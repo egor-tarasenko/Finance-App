ed70bb6bca699765ee897b7b0700c6922dadf12c
Egor Tarasenko <tarasenkoe324@gmail.com>
2024-01-08 18:52:47 +0200

    Instaling packages

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="btn btn-primary">
+    sdfsdfs <font-awesome-icon icon="file" />
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import {createPinia} from "pinia";
+import App from './App.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
+
+
+createApp(App).use(createPinia)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
